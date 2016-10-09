95aee678c0915db006c0fcd0d859f9cee71a5ef9
Brian Dorton <dorton@gmail.com>
2016-10-09 16:39:09 -0500

    adds a spelling bee boolean to words

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161008045851) do
+ActiveRecord::Schema.define(version: 20161009200740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,10 @@ ActiveRecord::Schema.define(version: 20161008045851) do
   create_table "words", force: :cascade do |t|
     t.string   "letters"
     t.string   "pic"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.integer  "week_id"
+    t.boolean  "spelling_bee", default: false
   end
 
 end
