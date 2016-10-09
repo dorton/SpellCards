9624ec86b96c27817514549a1b2734f19f14a8a5
Brian Dorton <dorton@gmail.com>
2016-10-09 16:32:44 -0500

    tries to add fix to turbolinks and js bootstrap dropdown

--- app/assets/javascripts/sitewide.js
+++ app/assets/javascripts/sitewide.js
@@ -1,18 +1,20 @@
 $( document ).on('turbolinks:load', function() {
-  $(".button-collapse").sideNav({
-    edge: 'right', // Choose the horizontal origin
-    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-  });
+  // $(".button-collapse").sideNav({
+  //   edge: 'right', // Choose the horizontal origin
+  //   closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  // });
+  //
+  // $('.dropdown-button').dropdown({
+  //     inDuration: 300,
+  //     outDuration: 225,
+  //     constrain_width: false, // Does not change width of dropdown to that of the activator
+  //     hover: true, // Activate on hover
+  //     gutter: 0, // Spacing from edge
+  //     belowOrigin: false, // Displays dropdown below the button
+  //     alignment: 'left' // Displays dropdown with edge aligned to the left of button
+  //   }
+  // );
 
-  $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: false, // Displays dropdown below the button
-      alignment: 'left' // Displays dropdown with edge aligned to the left of button
-    }
-  );
+  $('.dropdown-toggle').dropdown('toggle')
 
 })
