a42529f2a8a6499b05c8b69c2a774a9033686588
Brian Dorton <dorton@gmail.com>
2016-10-09 07:37:39 -0500

    new UI for the navbar and flip cards

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -1,31 +1,8 @@
 $( document ).on('turbolinks:load', function() {
 
+  $('.card').click(function(){
+    $(this).toggleClass('flipped');
 
-  // document.querySelector("#myCard").classList.toggle("flip")
-
-    // Initialisation de Masonry
-    // $("div.article").masonry({
-    //     itemSelector: "article",
-    //     isFitWidth: true
-    // }).masonry('on', 'layoutComplete', function (article) {
-    //     $("article").removeClass("z-depth-2").css("z-index", 0);
-    // });
-
-    // Affichage des informations
-    // $("button.say").click(function () {
-    //     $(this).parent("article").addClass("z-depth-2").css("z-index", 10);
-    //     $(this).siblings("ul.info").stop().slideToggle(function () {
-    //         var $article = $(this).parent("article");
-    //         $("div.content").masonry();
-    //     });
-    // });
-
-    // Calcul du prix
-    // $("input.quantity").on("change", function () {
-    //     $(this).siblings(".buy").children(".price").text($(this).val() * $(this).parent("article").data("price"));
-    // });
-    //
-    // // Forcer le calcul du prix à l'ouverture de la  page
-    // $("input.quantity").trigger("change");
+  });
 
   })
